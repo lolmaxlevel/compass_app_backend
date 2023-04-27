f052a666c095ee151a5433af787e942988a4c7c1
Ternovkiy Ilya <lolmaxlevel@gmail.com>
2023-04-27 20:27:53 +0300

    ?

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 var webSocketServer = new (require('ws')).Server({port: 9000}),
     webSockets = {} // userID: webSocket
-const express = require('express');
-const PORT = process.env.PORT || 3000;
-const INDEX = '/index.html';
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
-  
 console.log("server started on :9000")
 
 webSocketServer.on('connection', function connection(webSocket, req) {
