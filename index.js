8aa8792b8f97c60e9cc78c7395a7c60c3bbe0bf9
Ternovkiy Ilya <lolmaxlevel@gmail.com>
2023-04-27 17:54:51 +0300

    trying to deploy

--- index.js
+++ index.js
@@ -1,28 +1,15 @@
 var webSocketServer = new (require('ws')).Server({port: 9000}),
     webSockets = {} // userID: webSocket
+const express = require('express');
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
 
-// CONNECT /:userID
-// wscat -c ws://localhost:5000/1
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+  
 console.log("server started on :9000")
-const { networkInterfaces } = require('os');
 
-const nets = networkInterfaces();
-const results = Object.create(null); // Or just '{}', an empty object
-
-for (const name of Object.keys(nets)) {
-    for (const net of nets[name]) {
-        // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
-        // 'IPv4' is in Node <= 17, from 18 it's a number 4 or 6
-        const familyV4Value = typeof net.family === 'string' ? 'IPv4' : 4
-        if (net.family === familyV4Value && !net.internal) {
-            if (!results[name]) {
-                results[name] = [];
-            }
-            results[name].push(net.address);
-        }
-    }
-}
-console.log(results)
 webSocketServer.on('connection', function connection(webSocket, req) {
     webSocket.upgradeReq = req;
     var userID = parseInt(webSocket.upgradeReq.url.slice(1,webSocket.upgradeReq.url.size), 10)
