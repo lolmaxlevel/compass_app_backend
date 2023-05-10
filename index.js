16b66b77725d6dd3a4df594eef42161da832787e
Ternovkiy Ilya <lolmaxlevel@gmail.com>
2023-05-10 17:11:55 +0300

    fully rewritten server, works fine now

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 var webSocketServer = new (require('ws')).Server({port: 9000}),
     webSockets = {} // userID: webSocket
 
+connections = {}
 console.log("server started on :9000")
 
 webSocketServer.on('connection', function connection(webSocket, req) {
-    webSocket.upgradeReq = req;
-    var userID = parseInt(webSocket.upgradeReq.url.slice(1,webSocket.upgradeReq.url.size), 10)
-    webSockets[userID] = webSocket
-    console.log('connected: ' + userID + ' in ' + Object.getOwnPropertyNames(webSockets))
-
-    // Forward Message
-    //
-    // Receive               Example
-    // [toUserID, text]      [2, "Hello, World!"]
-    //
-    // Send                  Example
-    // [fromUserID, text]    [1, "Hello, World!"]
+	console.log("new user")
     webSocket.on('message', function(message) {
-        console.log('received from ' + userID + ': ' + message)
-        var request = JSON.parse(message)
-        var toUserWebSocket = webSockets[request['partner_id']]
-        if (toUserWebSocket) {
-            console.log('sent to ' + request['partner_id'])
-            toUserWebSocket.send(JSON.stringify(message))
-        }
+		let request = JSON.parse(message)
+		console.log('new request: ' + request['type'])
+		if (request['type'].toLowerCase() === 'handshake'){
+			console.log("handshake with " + request['id'])
+			webSockets[request['id']] = webSocket
+		}
+		if (request['type'].toLowerCase() === 'compass'){
+			if (request['status'] === 'start'){
+				connections[request['partnerId']] = request['id']
+				console.log(`new connection with ${request['partnerId']} : ${request['id']}`)
+			}
+		}
+		if (request['type'].toLowerCase() === 'location'){
+			console.log("got location:" + request['id'])
+			if (connections[request['id']] !== undefined && webSockets[connections[request['id']]] !== undefined){
+				webSockets[connections[request['id']]].send(JSON.stringify(request))
+			}
+		}
     })
 
     webSocket.on('close', function () {
-        delete webSockets[userID]
-        console.log('deleted: ' + userID)
+        console.log('deleted')
     })
 })
